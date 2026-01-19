3d019e272c87369d58d4f0645aaf24c8a8aea588
anndrzq <anndrzq32@gmail.com>
2026-01-19 22:16:55 +0700

    Solved Bug

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     // public const HOME = '/dashboard';
-    public const HOME = '/UserData';
+    public const HOME = '/Dashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
